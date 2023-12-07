ab5a3dc5e32f2edd86047a6866342f3bb27a6eaa
laihaojie <laihaojie@users.noreply.github.com>
2023-12-07 05:58:54 +0000

    deploy: laihaojie/jie-docs@9a8a807999ec9df620988795e564979bcfa70383

---
+++ docs/assets/src_notes_npmrc.md.gv0D9Kqt.js
@@ -0,0 +1,13 @@
+import{_ as s,o as n,c as a,R as e}from"./chunks/framework.dAbdJVy_.js";const _=JSON.parse('{"title":"npmrc","description":"","frontmatter":{},"headers":[],"relativePath":"src/notes/npmrc.md","filePath":"src/notes/npmrc.md","lastUpdated":1701928649000}'),p={name:"src/notes/npmrc.md"},r=e(`<h1 id="npmrc" tabindex="-1">npmrc <a class="header-anchor" href="#npmrc" aria-label="Permalink to &quot;npmrc&quot;">​</a></h1><div class="language-txt vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">txt</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span># 是否严格比较同等级依赖关系 默认为true</span></span>
+<span class="line"><span>strict-peer-dependencies=false</span></span>
+<span class="line"><span>ignore-workspace-root-check=true</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span># 用pnpm时是否扁平化node_modules</span></span>
+<span class="line"><span>node-linker=hoisted</span></span>
+<span class="line"><span>shamefully-hoist=true</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span># 换源</span></span>
+<span class="line"><span># 淘宝源</span></span>
+<span class="line"><span>registry=https://registry.npmmirror.com/</span></span>
+<span class="line"><span># npm源</span></span>
+<span class="line"><span>registry=https://registry.npmjs.org/</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br></div></div>`,2),l=[r];function c(t,i,o,m,d,b){return n(),a("div",null,l)}const h=s(p,[["render",c]]);export{_ as __pageData,h as default};
